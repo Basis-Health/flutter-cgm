6a54ce710b58df5a7d835b1e2aa50b0dda88f6c9
Michael Jajou <michaeljajouapps@gmail.com>
2023-02-17 15:38:44 -0500

    [CGMReading] - Use Enum for State

--- lib/lib/cgm_reading.dart
+++ lib/lib/cgm_reading.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
 part of cg_monitor;
 
 class CGMReading {
   final String uid;
-  final String state;
+  final CGMState state;
   final String raw;
   final String patchUID;
   final String? history;
@@ -22,12 +24,27 @@ class CGMReading {
   factory CGMReading.fromJson(Map<String, dynamic> json) {
     return CGMReading(
       uid: json['uid'],
-      state: json['state'],
+      state: CGMState.fromString(json['state']),
       raw: json['raw'],
       patchUID: json['patchUID'],
       history: json['history'],
       trend: json['trend'],
       minutesSinceStart: json['minutesSinceStart'],
     );
+  }
+}
+
+enum CGMState {
+  NEW,
+  ACTIVATING,
+  UNKNOWN,
+  OPERATIONAL;
+
+  static CGMState fromString(String val) {
+    try {
+      return CGMState.values.firstWhere((e) => e.name == val);
+    } catch (_) {
+      return CGMState.UNKNOWN;
+    }
   }
 }
